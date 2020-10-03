b5b2372f46c06cdaf2e83b8f4bbe2c17a2b90679
llama-with-thumbs <vlad.filitovich@gmail.com>
2020-10-03 15:56:28 -0400

    init: start Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  let arr = arguments.length == 0 || array.length == 0 ? [0] : array
+  arr.sort((a, b) => a-b)
+  return arr[0];
 }
 
 exports.max = function max (array) {
+  let arr = arguments.length == 0 || array.length == 0 ? [0] : array
+  arr.sort((a, b) => b - a)
+  return arr[0];
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let arr = arguments.length == 0 || array.length == 0 ? [0] : array
+  let accumulator = 0 
+  arr.forEach(element => {
+      accumulator += element
+  })    
+  return arr.length == 0 ? 0 : accumulator/arr.length
 }
